9561be8a56d70048b64bb53033ca7b215a968a68
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-03-10 18:40:35 +0100

    setting up IDe

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <div className="overlay">
+      
+      </div>
+    </>
+  )
+}
+
+export default App
