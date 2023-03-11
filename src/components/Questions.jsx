7d046b808a725019ab710fa9cd9e2f67c81d9897
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-03-11 14:10:57 +0100

    minor changes

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 function Questions() {
     return (
         <>
-            <h1 t_h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur facere iste obcaecati </h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora illo perspiciatis obcaecati eum voluptatibus corporis dolores nam iusto! Tempore vero velit dolorum, placeat iste modi quod quam nemo dolor laudantium?</p>
+            <h1 className="t_h2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur facere iste obcaecati </h1>
+            <p className="t_p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora illo perspiciatis obcaecati eum voluptatibus corporis dolores nam iusto! Tempore vero velit dolorum, placeat iste modi quod quam nemo dolor laudantium?</p>
         </>
     )
 }
