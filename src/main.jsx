05a641802103a70fc873f840435c028dec5b7ab2
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-03-13 16:04:03 +0100

    removed padding

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
